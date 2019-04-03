f1a33ea78702bd1f57f49ea8e4dc589d5f444082
michaeldaniello <ocrmike@gmail.com>
2019-04-03 12:23:18 -0400

    Removing chatroom

--- acadella-chat/app/main/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-from . import main
-
-
-@main.route('/', methods=['GET', 'POST'])
-def index():
-    """Main route which will prompt for login"""
-    return render_template('index.html')
-
