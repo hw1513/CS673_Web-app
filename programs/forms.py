5599d1698b789ee88563152f18cb8248f1ac1453
hw1513 <46538118+hw1513@users.noreply.github.com>
2019-04-04 16:18:12 -0400

    Add files via upload

--- programs/forms.py
+++ programs/forms.py
@@ -4,5 +4,5 @@ from .models import Programs
 class ProgramForm(forms.ModelForm):
 	class Meta:
 		model = Programs
-		fields= ["project_name", "start_date","project_description",]
+		fields= ["project_name", "start_date","project_description","project_members"]
 		
--- programs/urls.py
+++ programs/urls.py
@@ -3,11 +3,9 @@ from . import views
 
 
 urlpatterns = [
-	path('', views.program),
-	path('<program_name>/', views.detail, name='detail'),
-	path('<program_name>/add_member/', views.add_member, name='add_member')
-
-	
-	
+	path('', views.program,name='programhome'),
+	path('addprogram/', views.addprogram, name='addprogram'),
+	path('detail/<program_name>', views.detail, name='detail'),
+	path('<program_name>/add_member/', views.add_member, name='add_member'),
 
 ]
