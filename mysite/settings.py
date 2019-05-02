347462b5778cbc1bac94b08d981d3b0601817120
michaeldaniello <ocrmike@gmail.com>
2019-05-02 00:04:42 -0400

    Adding required heroku files

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,16 +80,6 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'test',
-        'USER': 'root',
-        'PASSWORD': '4447474wim',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -138,8 +128,13 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            #"hosts":[os.environ.get('REDIS_URL', 'redis://localhost:6379')],
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts":[os.environ.get('REDIS_URL')]
+            # "hosts": [('127.0.0.1', 6379)],
         },
     },
 }
+
+import django_heroku 
+
+django_heroku.settings(locals())
+del DATABASES['default']['OPTIONS']['sslmode']
