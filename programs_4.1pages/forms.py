104305cf26a9f42cd54c021c1fdf6466b45bfbda
michaeldaniello <ocrmike@gmail.com>
2019-05-02 00:29:22 -0400

    Removed migrations and excess files:

--- programs_4.1pages/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Programs
-
-class ProgramForm(forms.ModelForm):
-	class Meta:
-		model = Programs
-		fields= ["project_name", "start_date","project_description","project_members"]
-		
